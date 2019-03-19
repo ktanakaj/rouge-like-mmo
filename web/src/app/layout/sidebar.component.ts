8f8f1e6eafdec10cfa368d1e52281dc7199e3a57
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2019-03-19 18:50:58 +0900

    サイドバーをグループ化できるようにしてマスタ系のリンクを集約, スマホ幅にも仮対応

--- web/src/app/layout/sidebar.component.ts
+++ web/src/app/layout/sidebar.component.ts
@@ -6,6 +6,17 @@ import { filter } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+interface NaviItem {
+	title: string;
+	path: string;
+	active?: boolean;
+}
+
+interface NaviGroup {
+	title: string;
+	items: NaviItem[];
+}
+
 /**
  * サイドバーコンポーネントクラス。
  */
@@ -16,34 +27,33 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class SidebarComponent implements OnInit {
 	/** ナビ定義 */
-	navi: { title: string, path: string, active?: boolean }[][] = [
-		[
-			{
-				title: 'PLAYER_PAGE.LINK',
-				path: '/players',
-			},
-		],
-		[
-			{
-				title: 'MASTER_VERSION_PAGE.LINK',
-				path: '/masters',
-			},
-			{
-				title: 'MASTER_VIEWER_PAGE.LINK',
-				path: '/masters/latest',
-			},
-		],
-		[
-			{
-				title: 'ADMINISTRATOR_PAGE.LINK',
-				path: '/admin',
-			},
-		],
+	navi: (NaviItem | NaviGroup)[] = [
+		{
+			title: 'PLAYER_PAGE.LINK',
+			path: '/players',
+		},
+		{
+			title: 'MASTER_PAGE.LINK',
+			items: [
+				{
+					title: 'MASTER_VERSION_PAGE.LINK',
+					path: '/masters',
+				},
+				{
+					title: 'MASTER_VIEWER_PAGE.LINK',
+					path: '/masters/latest',
+				},
+			],
+		},
+		{
+			title: 'ADMINISTRATOR_PAGE.LINK',
+			path: '/admin',
+		},
 	];
 
 	/**
 	 * サービスをDIしてコンポーネントを生成する。
-	 * @param route ルート情報。
+	 * @param router ルーター。
 	 */
 	constructor(
 		private router: Router) { }
@@ -53,6 +63,7 @@ export class SidebarComponent implements OnInit {
 	 */
 	ngOnInit(): void {
 		// 画面移動に応じて現在値選択
+		// TODO: URL直打ち時に、初期画面が選択中にならない？
 		this.router.events.pipe(filter(ev => ev instanceof NavigationEnd)).subscribe((ev: NavigationEnd) => {
 			this.activateNavi(ev.url);
 		});
@@ -63,9 +74,15 @@ export class SidebarComponent implements OnInit {
 	 * @param path 表示中のパス。
 	 */
 	activateNavi(path: string): void {
+		// ※ さらにIDなどが付くことがあるので前方一致で比較。
+		// TODO: マスタ系ページが正しく判定できないので要改善。
 		for (let i = 0; i < this.navi.length; i++) {
-			for (let j = 0; j < this.navi[i].length; j++) {
-				this.navi[i][j].active = this.navi[i][j].path === path;
+			if (Array.isArray(this.navi[i]['items'])) {
+				for (let j = 0; j < this.navi[i]['items'].length; j++) {
+					this.navi[i]['items'][j].active = path.startsWith(this.navi[i]['items'][j].path);
+				}
+			} else {
+				this.navi[i]['active'] = path.startsWith(this.navi[i]['path']);
 			}
 		}
 	}
